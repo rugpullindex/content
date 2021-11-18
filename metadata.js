4757debc6f125dcb82b33135403c959c6e7f2045
Tim Daubenschütz <tim@daubenschuetz.de>
2021-11-18 16:46:23 +0100

    Port sitemap.xml structure to metadata file

--- metadata.js
+++ metadata.js
@@ -1,4 +1,7 @@
 // @format
+const { statSync } = require("fs");
+const path = require("path");
+
 module.exports = {
   sites: {
     _default: {
@@ -8,33 +11,77 @@ module.exports = {
     "/": {
       title: "Best Data Sets on Ethereum",
       description:
-        "A list of data sets sold on Ethereum ranked by their popularity and decentralization."
+        "A list of data sets sold on Ethereum ranked by their popularity and decentralization.",
+      sitemap: {
+        lastMod: true,
+        changeFreq: "hourly"
+      }
+    },
+    "/changelog.txt": {
+      sitemap: {
+        lastMod: "never"
+      }
     },
     "/about": {
-      title: "About Us"
-    },
-    "/docs": {
-      title: "API"
-    },
-    "/faq": {
-      title: "FAQ"
+      title: "About Us",
+      sitemap: {
+        lastMod: statSync(path.resolve(__dirname, "about.md")).mtime,
+        changeFreq: "daily"
+      }
     },
     "/blog": {
-      title: "Blog",
-      katex: true
+      sitemap: {
+        lastMod: "never"
+      }
+    },
+    "/docs": {
+      title: "API",
+      sitemap: {
+        lastMod: statSync(path.resolve(__dirname, "docs.md")).mtime,
+        changeFreq: "daily"
+      }
+    },
+    "/faq": {
+      title: "FAQ",
+      sitemap: {
+        lastMod: statSync(path.resolve(__dirname, "faq.md")).mtime,
+        changeFreq: "daily"
+      }
     },
     "/specification": {
       title: "Specification - How is RPI ranking its data sets",
-      katex: true
+      katex: true,
+      sitemap: {
+        lastMod: statSync(path.resolve(__dirname, "specification.md")).mtime,
+        changeFreq: "daily"
+      }
     },
     "/ocean-staking-risks-and-rewards-explained": {
       title: "Ocean Staking Risks & Rewards Explained",
-      katex: false
+      katex: false,
+      sitemap: {
+        lastMod: statSync(
+          path.resolve(
+            __dirname,
+            "ocean-staking-risks-and-rewards-explained.md"
+          )
+        ).mtime,
+        changeFreq: "daily"
+      }
     },
     "/ocean-staking-faq-everything-you-should-know-about-staking-your-ocean-tokens": {
       title:
         "Ocean Staking FAQ - Everything you should know about staking your OCEAN tokens",
-      katex: false
+      katex: false,
+      sitemap: {
+        lastMod: statSync(
+          path.resolve(
+            __dirname,
+            "ocean-staking-faq-everything-you-should-know-about-staking-your-ocean-tokens.md"
+          )
+        ).mtime,
+        changeFreq: "daily"
+      }
     }
   }
 };
